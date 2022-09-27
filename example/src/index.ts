111aef4d9d186bf4a9202d44be12251f7e13b1ca
Googlefan256 <yyykakeru@gmail.com>
2022-09-27 17:25:52 +0900

    examples

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,4 +1,4 @@
-import { Instance } from "../../dist";
+import { Instance } from "../../";
 
 (async () => {
   const app = new Instance();
--- example/src/routes/index.ts
+++ example/src/routes/index.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Router } from "../../../";
 
 export default Router().get("/", (_, res) => {
   res.end("Ok from /");
