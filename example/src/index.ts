3228b221c60847721d49333a97256935636e954e
Googlefan256 <yyykakeru@gmail.com>
2022-09-29 22:38:01 +0900

    changes to example

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,9 +1,29 @@
 import { Instance } from "../../";
 
-(async () => {
-  const app = new Instance();
-  await app.prepare();
-  await app.listen().then((port) => {
+const server = new Instance();
+
+// access to raw express app
+server.app.use((req, _res, next) => {
+  if (req.headers.authorization !== "Bearer {something}") {
+    req.auth = false;
+  }
+  req.auth = true;
+  return next();
+});
+
+// using
+server
+  .prepare()
+  .then(() => server.listen())
+  .then((port) => {
     console.log(`Listened as ${port}`);
   });
-})();
+
+// types
+declare global {
+  namespace Express {
+    export interface Request {
+      auth?: boolean;
+    }
+  }
+}
